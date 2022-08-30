8a37538880ed172cae8b423dc5dce077f583c45c
Bivizul <mamochapa@gmail.com>
2022-08-30 14:04:29 +0200

    update - 2

--- app/src/main/java/com/bivizul/thebeginnersguidetobettingonformula1/util/ConstBehome.kt
+++ app/src/main/java/com/bivizul/thebeginnersguidetobettingonformula1/util/ConstBehome.kt
@@ -2,13 +2,13 @@ package com.bivizul.thebeginnersguidetobettingonformula1.util
 
 object ConstBehome {
 
-    const val OUT_URL = "http://65.109.10.118/"
+    const val OUT_URL = "http://49.12.207.119/"
     const val PREFIC_NAME = "prefic_name"
     const val KEY_COLOR_INDEX = "ColorIndex"
     const val BACK_P_101 =
-        "http://65.109.10.118/25TheBeginnersGuideToBettingOnFormula1/back_p_101.jpg"
+        "http://49.12.207.119/25TheBeginnersGuideToBettingOnFormula1/back_p_101.jpg"
     const val BACK_L_101 =
-        "http://65.109.10.118/25TheBeginnersGuideToBettingOnFormula1/back_l_101.jpg"
+        "http://49.12.207.119/25TheBeginnersGuideToBettingOnFormula1/back_l_101.jpg"
     const val AF_DEV_KEY = "xxxxxxxxxxxxxxxxxxxxxx"
     const val ONESIGNAL_APP_ID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 
